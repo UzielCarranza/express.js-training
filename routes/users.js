ab53ee272852d67f6e4c932692f5ab5acf6f76e3
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-29 10:01:42 -0400

    sending data to the endpoint GET

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var data = require('../data/MOCK_DATA.json')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.json(data)
 });
 
 module.exports = router;
