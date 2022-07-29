6f39dec4c59bdcbe7d64654ced15af093a368d83
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-29 10:52:41 -0400

    using method GET with routing parameters to send to the client a student entry that corresponds with that parameter

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/MOCK_DATA.json');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 
-router.get('/', function(request, response, next) {
-  response.render('index', {title: "response from /"});
+// router.get('/', function(request, response, next) {
+//   response.render('index', {title: "response from /"});
+// });
+
+//GET with routing parameters
+
+
+router.get('/class/:id', function(request, response, next) {
+  // response.render('index', {title: "response from /"});
+  const studentId = Number(request.params.id);
+  const students = data.filter((student) => student.id === studentId);
+  response.send(students)
 });
+
 
 module.exports = router;
